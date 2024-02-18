5fac3b7967bf2b17d4adb77d8317edf7646c8b6b
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2024-02-18 16:31:28 +0200

    task 1-3 is done

--- src/2.ts
+++ src/2.ts
@@ -15,12 +15,8 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  // Заповніть модифікатори доступу// +
+  constructor(public name: string, private department: string, protected salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +29,11 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary = salary + 10000;
+  }
 }
 
 
-export {};
+export { };
